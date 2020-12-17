1be60748f92281f7cdee5b4a4791e37b2e15d89a
Mayank Aggarwal <mayank.aggarwal@allaboutcake.com>
2020-12-17 20:40:21 +0100

    Initial Commit

---
+++ src/test/kotlin/org/appcode/basic/BasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.appcode.basic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
